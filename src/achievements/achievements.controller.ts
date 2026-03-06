00118af5179ac616e756cc2e57125e6b45ca6ee6
Eorico Gonzales <eoricogonzales@gmail.com>
2026-03-06 19:40:21 +0800

    achievements done

--- src/achievements/achievements.controller.ts
+++ src/achievements/achievements.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { AchievementsService } from './achievements.service';
+import { Achievements } from 'src/schemas/schemas';
 
 @Controller('achievements')
-export class AchievementsController {}
+export class AchievementsController {
+    constructor(private readonly achievementsService: AchievementsService) {}
+    
+    @Get()
+    getAll() {
+        return this.achievementsService.getAll();
+    }
+
+    @Post()
+    create(@Body() data: Achievements) {
+        return this.achievementsService.create(data);
+    }
+
+    @Put(':id')
+    update(@Param() id: string, @Body() data: Achievements) {
+        return this.achievementsService.update(id, data);
+    }
+
+    @Delete(':id')
+    delete(@Param() id: string) {
+        return this.achievementsService.delete(id)
+    }
+}
--- src/achievements/achievements.service.ts
+++ src/achievements/achievements.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Achievements, AchievementsDocument } from 'src/schemas/schemas';
 
 @Injectable()
-export class AchievementsService {}
+export class AchievementsService {
+    constructor(@InjectModel(Achievements.name) private achievementsModel: Model<AchievementsDocument>) {}
+    
+    
+    async getAll(): Promise<Achievements[]> {
+        return this.achievementsModel.find().exec();
+    }
+
+    async create(about: Achievements): Promise<Achievements> {
+        const createdAbout = new this.achievementsModel(about);
+        return createdAbout.save();
+    }
+
+    async update(id: string, data: Partial<Achievements>): Promise<Achievements | null> {
+        return this.achievementsModel.findByIdAndUpdate(id, data, { new: true });
+    }
+
+    async delete(id: string): Promise<Achievements | null> {
+        return this.achievementsModel.findByIdAndDelete(id);
+    }
+}

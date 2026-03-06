e620a5a4e780adcaa1d47dc3cc7b1f90261c4989
Eorico Gonzales <eoricogonzales@gmail.com>
2026-03-06 19:48:21 +0800

    tech skills done

--- src/tech-skills/tech-skills.controller.ts
+++ src/tech-skills/tech-skills.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { TechSkillsService } from './tech-skills.service';
+import { TechSkills } from 'src/schemas/schemas';
 
 @Controller('tech-skills')
-export class TechSkillsController {}
+export class TechSkillsController {
+    constructor(private readonly techSkillsService: TechSkillsService) {}
+    
+    @Get()
+    getAll() {
+        return this.techSkillsService.getAll();
+    }
+
+    @Post()
+    create(@Body() data: TechSkills) {
+        return this.techSkillsService.create(data);
+    }
+
+    @Put(':id')
+    update(@Param() id: string, @Body() data: TechSkills) {
+        return this.techSkillsService.update(id, data);
+    }
+
+    @Delete(':id')
+    delete(@Param() id: string) {
+        return this.techSkillsService.delete(id)
+    }
+}
--- src/tech-skills/tech-skills.service.ts
+++ src/tech-skills/tech-skills.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { TechSkills, TechSkillsDocument } from 'src/schemas/schemas';
 
 @Injectable()
-export class TechSkillsService {}
+export class TechSkillsService {
+    constructor(@InjectModel(TechSkills.name) private techSkillsModel: Model<TechSkillsDocument>) {}
+      
+    async getAll(): Promise<TechSkills[]> {
+        return this.techSkillsModel.find().exec();
+    }
+
+    async create(about: TechSkills): Promise<TechSkills> {
+        const createdAbout = new this.techSkillsModel(about);
+        return createdAbout.save();
+    }
+
+    async update(id: string, data: Partial<TechSkills>): Promise<TechSkills | null> {
+        return this.techSkillsModel.findByIdAndUpdate(id, data, { new: true });
+    }
+
+    async delete(id: string): Promise<TechSkills | null> {
+        return this.techSkillsModel.findByIdAndDelete(id);
+    }
+}

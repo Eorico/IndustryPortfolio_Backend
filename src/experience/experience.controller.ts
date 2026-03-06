793dae1b43297ea7996980046bc5ac21f3a94705
Eorico Gonzales <eoricogonzales@gmail.com>
2026-03-06 19:45:56 +0800

    experience done

--- src/experience/experience.controller.ts
+++ src/experience/experience.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ExperienceService } from './experience.service';
+import { Experience } from 'src/schemas/schemas';
 
 @Controller('experience')
-export class ExperienceController {}
+export class ExperienceController {
+    constructor(private readonly experiencesService: ExperienceService) {}
+    
+    @Get()
+    getAll() {
+        return this.experiencesService.getAll();
+    }
+
+    @Post()
+    create(@Body() data: Experience) {
+        return this.experiencesService.create(data);
+    }
+
+    @Put(':id')
+    update(@Param() id: string, @Body() data: Experience) {
+        return this.experiencesService.update(id, data);
+    }
+
+    @Delete(':id')
+    delete(@Param() id: string) {
+        return this.experiencesService.delete(id)
+    }
+}
--- src/experience/experience.service.ts
+++ src/experience/experience.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Experience, ExperienceDocument } from 'src/schemas/schemas';
 
 @Injectable()
-export class ExperienceService {}
+export class ExperienceService {
+    constructor(@InjectModel(Experience.name) private experiencesModel: Model<ExperienceDocument>) {}
+
+
+    async getAll(): Promise<Experience[]> {
+        return this.experiencesModel.find().exec();
+    }
+
+    async create(about: Experience): Promise<Experience> {
+        const createdAbout = new this.experiencesModel(about);
+        return createdAbout.save();
+    }
+
+    async update(id: string, data: Partial<Experience>): Promise<Experience | null> {
+        return this.experiencesModel.findByIdAndUpdate(id, data, { new: true });
+    }
+
+    async delete(id: string): Promise<Experience | null> {
+        return this.experiencesModel.findByIdAndDelete(id);
+    }
+}

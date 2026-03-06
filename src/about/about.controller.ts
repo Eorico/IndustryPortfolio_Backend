a05071ffd4f551919104c2a2f3c78a01348d5877
Eorico Gonzales <eoricogonzales@gmail.com>
2026-03-06 19:35:53 +0800

    about module done

--- src/about/about.controller.ts
+++ src/about/about.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { AboutService } from './about.service';
+import { About } from 'src/schemas/schemas';
 
 @Controller('about')
-export class AboutController {}
+export class AboutController {
+    constructor(private readonly aboutService: AboutService) {}
+
+    @Get()
+    getAll() {
+        return this.aboutService.getAll();
+    }
+
+    @Post()
+    create(@Body() data: About) {
+        return this.aboutService.create(data);
+    }
+
+    @Put(':id')
+    update(@Param() id: string, @Body() data: About) {
+        return this.aboutService.update(id, data);
+    }
+
+    @Delete(':id')
+    delete(@Param() id: string) {
+        return this.aboutService.delete(id)
+    }
+}
--- src/about/about.service.ts
+++ src/about/about.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { About, AboutDocument } from 'src/schemas/schemas';
 
 @Injectable()
-export class AboutService {}
+export class AboutService {
+    constructor(@InjectModel(About.name) private aboutModel: Model<AboutDocument>) {}
+
+
+    async getAll(): Promise<About[]> {
+        return this.aboutModel.find().exec();
+    }
+
+    async create(about: About): Promise<About> {
+        const createdAbout = new this.aboutModel(about);
+        return createdAbout.save();
+    }
+
+    async update(id: string, data: Partial<About>): Promise<About | null> {
+        return this.aboutModel.findByIdAndUpdate(id, data, { new: true });
+    }
+
+    async delete(id: string): Promise<About | null> {
+        return this.aboutModel.findByIdAndDelete(id);
+    }
+}

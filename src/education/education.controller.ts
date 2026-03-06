644a88882be5bcb8f185d1cf7062e5bb06eac38d
Eorico Gonzales <eoricogonzales@gmail.com>
2026-03-06 19:43:14 +0800

    education done

--- src/education/education.controller.ts
+++ src/education/education.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { EducationService } from './education.service';
+import { Education } from 'src/schemas/schemas';
 
 @Controller('education')
-export class EducationController {}
+export class EducationController {
+    constructor(private readonly educationService: EducationService) {}
+    
+    @Get()
+    getAll() {
+        return this.educationService.getAll();
+    }
+
+    @Post()
+    create(@Body() data: Education) {
+        return this.educationService.create(data);
+    }
+
+    @Put(':id')
+    update(@Param() id: string, @Body() data: Education) {
+        return this.educationService.update(id, data);
+    }
+
+    @Delete(':id')
+    delete(@Param() id: string) {
+        return this.educationService.delete(id)
+    }
+}
--- src/education/education.service.ts
+++ src/education/education.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Education, EducationDocument } from 'src/schemas/schemas';
 
 @Injectable()
-export class EducationService {}
+export class EducationService {
+     constructor(@InjectModel(Education.name) private educationModel: Model<EducationDocument>) {}
+    
+    
+    async getAll(): Promise<Education[]> {
+        return this.educationModel.find().exec();
+    }
+
+    async create(about: Education): Promise<Education> {
+        const createdAbout = new this.educationModel(about);
+        return createdAbout.save();
+    }
+
+    async update(id: string, data: Partial<Education>): Promise<Education | null> {
+        return this.educationModel.findByIdAndUpdate(id, data, { new: true });
+    }
+
+    async delete(id: string): Promise<Education | null> {
+        return this.educationModel.findByIdAndDelete(id);
+    }
+}

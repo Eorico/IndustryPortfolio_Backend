2f5f729aeb4c30c1464cd2ab4cb2bb8d2002e2a7
Eorico Gonzales <eoricogonzales@gmail.com>
2026-03-15 12:54:23 +0800

    update the port

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,10 @@ async function bootstrap() {
   // global prefix
   app.setGlobalPrefix('portfolio');
 
+  const port = process.env.PORT ?? 3000;
+
+  // Bind to 0.0.0.0 so Render can detect the port
+  await app.listen(port, '0.0.0.0');
 
   Logger.log("Mongo DB Connected");
   Logger.log(`Server is now Live! at - https://industryportfolio-backend.onrender.com`);

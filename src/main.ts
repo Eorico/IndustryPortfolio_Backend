2c85054d4187ae8eac0cf4ef62759d7808ca1d32
Eorico Gonzales <eoricogonzales@gmail.com>
2026-03-15 12:08:53 +0800

    update again the endpoint

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "https://industryportfolio-backend.onrender.com",
+    origin: "https://industry-portfolio-jet.vercel.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
